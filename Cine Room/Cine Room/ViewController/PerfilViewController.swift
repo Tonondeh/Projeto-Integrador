a8437e94b780d808f2b63b87f2fa6f182d85e8cf
Danilo <duvara@gmail.com>
2021-02-25 21:26:44 -0300

    story board e viewcontroler da tela perfil

--- Cine Room/Cine Room/ViewController/PerfilViewController.swift
+++ Cine Room/Cine Room/ViewController/PerfilViewController.swift
@@ -6,24 +6,122 @@
 //
 
 import UIKit
+import Foundation
 
 class PerfilViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
 
-    /*
-    // MARK: - Navigation
+    @IBOutlet weak var perfilImageView: UIImageView!
+    @IBOutlet weak var nomeUsuarioLabel: UILabel!
+    @IBOutlet weak var nomeCompletoLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var cpfLabel: UILabel!
+    @IBOutlet weak var dataNascimentoLabel: UILabel!
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var nomeCompletoTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var cpfTextField: UITextField!
+    @IBOutlet weak var nascimentoDataPicker: UIDatePicker!
+    @IBOutlet weak var editarButton: UIBarButtonItem!
+    @IBOutlet weak var mudarFotoButton: UIButton!
+    @IBOutlet weak var salvarButton: UIButton!
+    
+    var imagePicker:UIImagePickerController = UIImagePickerController()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mudarFotoButton.isHidden = true
+        self.salvarButton.isHidden = true
+        self.configTextField()
+        self.configImagePicker()
+        
+        self.salvarButton.layer.cornerRadius = 5
+        
+        
+        self.nomeTextField.isEnabled = false
+        self.nomeCompletoTextField.isEnabled = false
+        self.emailTextField.isEnabled = false
+        self.cpfTextField.isEnabled = false
+        self.nascimentoDataPicker.isEnabled = false
+        
+        
+        
+
     }
-    */
+    
+    func configImagePicker(){
+        self.imagePicker.delegate = self
+    }
+    
+    func configTextField(){
+        self.emailTextField.delegate = self
+        self.nomeTextField.delegate = self
+        self.nomeCompletoTextField.delegate = self
+        self.cpfTextField.delegate = self
+        
+        
+    }
+    
+    
 
+    @IBAction func editPerfilTappedButton(_ sender: UIBarButtonItem) {
+        self.mudarFotoButton.isHidden = false
+        self.salvarButton.isHidden = false
+        
+        self.nomeTextField.isEnabled = true
+        self.nomeCompletoTextField.isEnabled = true
+        self.cpfTextField.isEnabled = true
+        self.nascimentoDataPicker.isEnabled = true
+        
+        
+    }
+    
+    @IBAction func salvarTappedButton(_ sender: UIButton) {
+        self.mudarFotoButton.isHidden = true
+        self.salvarButton.isHidden = true
+        
+        self.nomeTextField.isEnabled = false
+        self.nomeCompletoTextField.isEnabled = false
+        self.cpfTextField.isEnabled = false
+        self.nascimentoDataPicker.isEnabled = false
+        }
+    
+    @IBAction func editPhotoTappedButton(_ sender: UIButton) {
+        self.imagePicker.sourceType = .photoLibrary
+        self.present(self.imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    
 }
+
+extension PerfilViewController: UITextFieldDelegate {
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.blue.cgColor
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
+}
+
+extension PerfilViewController: UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagemRecuperada = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        self.perfilImageView.image = imagemRecuperada ?? UIImage()
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+
